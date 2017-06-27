0142c9d8ad1965bf3efe0a2a32d1b544d7103221
Podcast Linux <podcastlinux@gmail.com>
2017-06-27 09:10:57 +0100

    Create 2017-06-28-#18-Linux-Express.m

---
+++ _posts/2017-06-28-#18-Linux-Express.m
@@ -0,0 +1,37 @@
+---
+layout: post
+title: "#18 Linux Express"
+date: 2017-06-28
+categories: LinuxExpress
+image: images/linuxexpress.png
+podcast_link: https://github.com/podcastlinux/podcastlinux.github.io/raw/master/Linux-Express/%2318%20Podcast%20Linux%20Express.mp3
+tags: [audio, telegram, Linux Express,]
+comments: true
+---
+
+Otro Linux Express para que estés informado del día a día de Podcast Linux.
+![#17](https://podcastlinux.github.io/images/18LinuxExpress.png)
+
+<audio controls>
+  <source src="https://github.com/podcastlinux/podcastlinux.github.io/raw/master/Linux-Express/%2318%20Podcast%20Linux%20Express.mp3" type="audio/mpeg">
+</audio>
+
+Repasamos lo acontecido en estas semanas:
+
++ [Episodio #27 Especial Slimbook One](http://avpodcast.net/podcastlinux/slimbookone)
++ Especial 1º Aniversario Podcast Linux 1 de julio
++ Próximo episodio Linux Connexion con Alejandro López[Slimbook](https://slimbook.es)
++ [Premios Asociación Podcast y directo](http://jpod.es/)
++ Aterrizan los Chromebooks en Educación
+
+
+Recuerda que puedes **contactar** conmigo de las siguientes formas:
+
++ Twitter: <https://twitter.com/podcastlinux>
++ Correo: <podcastlinux@avpodcast.net>
++ Web: <http://avpodcast.net/podcastlinux/>
++ Blog: <https://podcastlinux.github.io/>
++ Telegram: <https://t.me/podcastlinux>
++ Youtube: <https://www.youtube.com/PodcastLinux>
++ Feed Podcast Linux: <https://feedpress.me/podcastlinux>
++ Feed Linux Express (Audios Telegram): <https://podcastlinux.github.io/Linux-Express/feed>
